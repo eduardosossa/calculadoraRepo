ff6af567cca5f99bebbe12a6e52138204310fecc
Eduardo Sossa <Eduardo Sossa@DESKTOP-812FA3V>
2021-01-14 19:57:05 -0400

    funciona suma en get y post

---
+++ ProyectoCalculadora/Properties/controllers/CalculadoraController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProyectoCalculadora.Properties.controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class CalculadoraController : Controller
+    {
+        [HttpGet]
+        public int suma(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        [HttpPost]
+        public int sumar([FromHeader] int num1, [FromHeader] int num2)
+        {
+            return num1 + num2;
+        }
+    }
+}
