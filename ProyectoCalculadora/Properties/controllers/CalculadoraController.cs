b2856696e8fa77b53093a9a1e1191563061b1249
Eduardo Sossa <Eduardo Sossa@DESKTOP-812FA3V>
2021-01-14 21:12:52 -0400

    resta, divicin, multiplicacion en get y post

--- ProyectoCalculadora/Properties/controllers/CalculadoraController.cs
+++ ProyectoCalculadora/Properties/controllers/CalculadoraController.cs
@@ -11,14 +11,52 @@ namespace ProyectoCalculadora.Properties.controllers
     public class CalculadoraController : Controller
     {
         [HttpGet]
-        public int suma(int num1, int num2)
+        [Route("suma")]
+        public int sumaget(int num1, int num2)
         {
             return num1 + num2;
         }
         [HttpPost]
-        public int sumar([FromHeader] int num1, [FromHeader] int num2)
+        [Route("suma")]
+        public int sumapost([FromHeader] int num1, [FromHeader] int num2)
         {
             return num1 + num2;
+        }
+        [HttpGet]
+        [Route("resta")]
+        public int restaget(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+        [HttpPost]
+        [Route("resta")]
+        public int restapost([FromHeader] int num1, [FromHeader] int num2)
+        {
+            return num1 - num2;
+        }
+        [HttpGet]
+        [Route("multi")]
+        public int multiget(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+        [HttpPost]
+        [Route("multi")]
+        public int multipost([FromHeader] int num1, [FromHeader] int num2)
+        {
+            return num1 * num2;
+        }
+        [HttpGet]
+        [Route("div")]
+        public float divget(float num1, float num2)
+        {
+            return num1 / num2;
+        }
+        [HttpPost]
+        [Route("div")]
+        public float divpost([FromHeader] float num1, [FromHeader] float num2)
+        {
+            return num1 / num2;
         }
     }
 }
